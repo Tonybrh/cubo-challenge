cd6221370fc639c679b1a72241a22c7b29f7ba8c
Antônio Dias <antonio.eduardo@voxtec.com.br>
2025-03-26 17:16:02 -0300

    feat: adding the models creation

---
+++ back/app/Models/Comment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'content',
+        'user_id',
+        'task_id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function task(): BelongsTo
+    {
+        return $this->belongsTo(Task::class);
+    }
+}
